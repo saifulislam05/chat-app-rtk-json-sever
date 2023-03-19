c01f3f9f124ba29b1b4260228c0339ac31177083
Saiful islam <saiful.bijulia@gmail.com>
2023-03-19 11:10:41 +0530

    changes

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const apiSlice = createApi({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({
+        baseUrl: process.env.REACT_APP_API_URL,
+        prepareHeaders: async (Headers, { getState, endpoints })=>{
+            const token = getState()?.auth?.accessToken;
+            if (token) {
+                Headers.set("Authoriation", `Bearer ${token}`);
+            }
+            return Headers
+        }
+    }),
+    tagTypes: [],
+    endpoints: (builder)=>({}),
+
+})
